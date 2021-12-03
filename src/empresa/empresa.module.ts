81d6a83666d4bfb724e6333bea6a4f6e3030a92f
João <hlabeats@gmail.com>
2021-12-03 15:23:11 -0300

    att modules

--- src/empresa/empresa.module.ts
+++ src/empresa/empresa.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { EmpresaService } from './empresa.service';
 import { EmpresaController } from './empresa.controller';
 import { PrismaService } from 'src/prisma.service';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [EmpresaController],
   providers: [EmpresaService, PrismaService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { PrismaService } from 'src/prisma.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [UsersController],
   providers: [UsersService, PrismaService],
 })
