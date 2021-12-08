43307a458bf3cea05a95f9c2835e578d3b46b344
João <hlabeats@gmail.com>
2021-12-08 18:02:24 -0300

    procfile

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
+  app.enableCors();
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
 }
