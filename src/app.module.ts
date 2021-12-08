d2fa831895a2534b894c311f545b23165c192168
João <hlabeats@gmail.com>
2021-12-08 18:20:34 -0300

    script update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { UsersModule } from './users/users.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { EmpresaModule } from './empresa/empresa.module';
 import { ProdutoModule } from './produto/produto.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 @Module({
   imports: [UsersModule, EmpresaModule, CategoriaModule, ProdutoModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
