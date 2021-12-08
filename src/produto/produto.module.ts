d1f8bab0d5ccc13b080c75b0ddb601f47b05bed0
João <hlabeats@gmail.com>
2021-12-08 20:37:06 -0300

    import

--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -2,6 +2,7 @@ import { PrismaService } from 'src/prisma.service';
 import { Module } from '@nestjs/common';
 import { ProdutoService } from './produto.service';
 import { ProdutoController } from './produto.controller';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
