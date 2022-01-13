8b3101925e93d4101e24cb9e02598415ca843aa7
amorajuno <amorajuno@gmail.com>
2022-01-13 19:17:38 -0300

    testing

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -11,6 +11,7 @@ export class ProductService {
   async create(data: Prisma.ProductCreateInput): Promise<Product> {
     const product = await this.db.product.create({ data });
     console.log(product);
+    console.log(data);
     return product;
   }
 
